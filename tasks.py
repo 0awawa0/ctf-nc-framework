fa2f67e5a5367535f410821baa216f9854044a10
0awawa0 <mail.0awawa0@gmail.com>
2021-01-08 14:59:53 +0300

    Added add_round_key and sbox tasks

--- tasks.py
+++ tasks.py
@@ -8,5 +8,11 @@ def get_task(task_name):
 		import src.hello_world
 		return src.hello_world.hello
 	elif task_name == "matrix2bytes":
-		import src.matrix2bytes.main
-		return src.matrix2bytes.main.main
+		import src.ctf_tasks.matrix2bytes.main
+		return src.ctf_tasks.matrix2bytes.main.main
+	elif task_name == "add_round_key":
+		import src.ctf_tasks.add_round_key.main
+		return src.ctf_tasks.add_round_key.main.main
+	elif task_name == "sbox":
+		import src.ctf_tasks.sbox.sbox
+		return src.ctf_tasks.sbox.sbox.main
